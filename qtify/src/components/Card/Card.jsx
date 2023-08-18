3991e6bdc098886931de60a2b3ce0f656b860962
ArpanMitra72 <arpanmitra72@gmail.com>
2023-08-18 19:51:48 +0530

    grid-qtify-takehome

--- qtify/src/components/Card/Card.jsx
+++ qtify/src/components/Card/Card.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./Card.module.css";
 import cardImage from "../../assets/firstcardImage.png";
 
 const Card = () => {
+  const [albumDataList, setAlbumDataList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://qtify-backend-labs.crio.do/albums/top")
+      .then((response) => {
+        const albums = response.data;
+        setAlbumDataList(albums);
+        console.log(albums);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
   return (
-    <div className={styles.fullCard}>
-      <div className={styles.onlyCard}>
-        <img src={cardImage} alt="Card Image" width={159} height={170} />
-        <div className={styles.settingSmallBox}>
-          <div className={styles.smallBox}>
-            <div className={styles.followersText}>100 Follows</div>
+    <div className={styles.albumsContainer}>
+      {albumDataList.map((albumData) => (
+        <div
+          className={`${styles.fullCard} ${styles.cardWithBorderRadius}`}
+          key={albumData.id}
+        >
+          <div className={styles.onlyCard}>
+            <img
+              src={albumData.image}
+              alt="Card Image"
+              width={159}
+              height={170}
+            />
+            <div className={styles.settingSmallBox}>
+              <div className={styles.smallBox}>
+                <div className={styles.followersText}>
+                  {albumData.follows} Follows
+                </div>
+              </div>
+            </div>
           </div>
+          <div className={styles.titleText}>{albumData.title}</div>
         </div>
-      </div>
-      <div className={styles.titleText}>New Bollywood</div>
+      ))}
     </div>
   );
 };
--- qtify/src/components/MainBody/MainBody.jsx
+++ qtify/src/components/MainBody/MainBody.jsx
@@ -6,6 +6,10 @@ const MainBody = () => {
   return (
     <div className={style.fullBody}>
       <div className={style.topBody}>
+        <div className={style.topAlbumText}>
+          <p class="firstText">Top Albums</p>
+          <p class="secondText">Show all</p>
+        </div>
         <Card />
       </div>
     </div>
